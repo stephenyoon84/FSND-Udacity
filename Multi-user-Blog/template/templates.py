52a6b9726abe2013cca7345e2e964b83de82a55b
Stephen <stephenyoon84@gmail.com>
2017-04-10 15:45:27 -0400

    Add hello_world (birthday input (form)) Add template folder

---
+++ Multi-user-Blog/template/templates.py
@@ -0,0 +1,15 @@
+import os
+
+import webapp2
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+class MainPage(Handler):
+    def get(self):
+        self.write("Hello, Udacidy!")
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
